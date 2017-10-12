4c7e1c686161a0a2e8aa944de922a2aeca883efb
Veerababu <veerababu.mummidi@cgi.com>
2017-10-12 15:15:01 +0530

    testing

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -4,6 +4,6 @@ export namespace HelloWorldEx {
     console.log('hi helloworld just starting..')
   }
   export function sayGoodbye() {
-    console.log('goodbye3333')
+    console.log('goodbye444')
   }
 }
