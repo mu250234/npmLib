a4c6d2970bcdb140e2e62b537722bde004561663
veerababu <veerababu.mummidi@cgi.com>
2017-10-12 15:44:13 +0300

    what I changed packe

--- dist/hello-world.js
+++ dist/hello-world.js
@@ -7,7 +7,7 @@ var HelloWorldEx;
     }
     HelloWorldEx.sayHello = sayHello;
     function sayGoodbye() {
-        console.log('goodbye02');
+        console.log('goodbye03');
     }
     HelloWorldEx.sayGoodbye = sayGoodbye;
 })(HelloWorldEx = exports.HelloWorldEx || (exports.HelloWorldEx = {}));
